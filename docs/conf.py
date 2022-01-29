31e3d68f427a08efbc558092a9b40bce06c59277
iacopo <iacopomarmorini@gmail.com>
2022-01-30 00:47:10 +0100

    docs: retry press theme

--- docs/conf.py
+++ docs/conf.py
@@ -80,8 +80,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-extensions.append("sphinxjp.themes.basicstrap")
-html_theme = 'basicstrap'
+html_theme = 'press'
 
 # html_logo = 'build/html/_static/logo.png'
 
