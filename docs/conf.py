96c8d425bf737f85784f0f57eaa91f02451bceb8
iacopo <iacopomarmorini@gmail.com>
2022-01-29 23:56:05 +0100

    docs: theme basicstrap

--- docs/conf.py
+++ docs/conf.py
@@ -80,8 +80,10 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'press'
-html_logo = 'build/html/_static/logo.png'
+extensions.append("sphinxjp.themes.basicstrap")
+html_theme = 'basicstrap'
+
+# html_logo = 'build/html/_static/logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
