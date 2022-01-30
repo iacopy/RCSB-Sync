2cb110a47a74e224df35f2555534169164ced7ed
iacopo <iacopomarmorini@gmail.com>
2022-01-30 12:29:40 +0100

    Enable the Markdown parser for Sphynx
    https://www.sphinx-doc.org/en/master/usage/markdown.html

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'myst_parser',  # Markdown parser
 ]
 
 # Add any paths that contain templates here, relative to this directory.
