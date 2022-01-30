685726150a16e9c13c252b6197d1b3bb4dc412ea
iacopo <iacopomarmorini@gmail.com>
2022-01-30 11:52:31 +0100

    docs/conf.py: emoji on the left

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'RCSB Sync 🦜'
+project = '🦜 RCSB Sync'
 copyright = '2022, Iacopo Marmorini'
 author = 'Iacopo Marmorini'
 
