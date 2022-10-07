51d661637b6a75fbfffe5e0c983eccbb5cfac6ce
iacopo <iacopomarmorini@gmail.com>
2022-10-07 12:56:41 +0200

    test_rcsbids.py: update and fix tests
    Add computational query with 2 results and experimental with zero results (discovering crash with 204)

--- src/test_rcsbids.py
+++ src/test_rcsbids.py
@@ -17,99 +17,119 @@ from rcsbids import load_pdb_ids
 from rcsbids import retrieve_pdb_ids
 from rcsbids import store_pdb_ids
 
-TEST_QUERY = _load_query('queries/test_query.json')
-# A real query, which returns 6 entries.
-EXPECTED_QUERY = """{"query":{"type":"group","logical_operator":"and","nodes":[{"type":"terminal","service":"text","parameters":{"attribute":"entity_poly.rcsb_entity_polymer_type","operator":"exact_match","negation":false,"value":"Protein"}},{"type":"terminal","service":"text","parameters":{"attribute":"exptl.method","operator":"exact_match","negation":false,"value":"THEORETICAL MODEL"}}],"label":"text"},"return_type":"entry","request_options":{"pager":{"start":0,"rows":25},"scoring_strategy":"combined","sort":[{"sort_by":"score","direction":"desc"}]}}"""
-
-# The server response to the query above.
-EXPECTED_JSON_RESPONSE = {
-    "query_id": "ed8fc6b7-7a04-4733-ba33-88740de1f970",
-    "result_type": "entry",
-    "total_count": 6,
-    "result_set": [{
-        "identifier": "1DWL",
-        "score": 1.0,
-        "services": [{
-            "service_type": "text",
-            "nodes": [{
-                "node_id": 31667,
-                "original_score": 10.145727157592773,
-                "norm_score": 1.0
-            }]
-        }]
-    }, {
-        "identifier": "1E08",
-        "score": 1.0,
-        "services": [{
-            "service_type": "text",
-            "nodes": [{
-                "node_id": 31667,
-                "original_score": 10.145727157592773,
-                "norm_score": 1.0
-            }]
-        }]
-    }, {
-        "identifier": "1GX7",
-        "score": 1.0,
-        "services": [{
-            "service_type": "text",
-            "nodes": [{
-                "node_id": 31667,
-                "original_score": 10.145727157592773,
-                "norm_score": 1.0
-            }]
-        }]
-    }, {
-        "identifier": "1OLN",
-        "score": 1.0,
-        "services": [{
-            "service_type": "text",
-            "nodes": [{
-                "node_id": 31667,
-                "original_score": 10.145727157592773,
-                "norm_score": 1.0
-            }]
-        }]
-    }, {
-        "identifier": "1UR6",
-        "score": 1.0,
-        "services": [{
-            "service_type": "text",
-            "nodes": [{
-                "node_id": 31667,
-                "original_score": 10.145727157592773,
-                "norm_score": 1.0
-            }]
-        }]
-    }, {
-        "identifier": "1VYC",
-        "score": 1.0,
-        "services": [{
-            "service_type": "text",
-            "nodes": [{
-                "node_id": 31667,
-                "original_score": 10.145727157592773,
-                "norm_score": 1.0
-            }]
-        }]
-    }]
+# Test data
+TEST_QUERY_EXP = """{
+  "query": {
+    "type": "terminal",
+    "label": "text",
+    "service": "text",
+    "parameters": {
+      "attribute": "rcsb_entity_source_organism.taxonomy_lineage.name",
+      "operator": "exact_match",
+      "negation": false,
+      "value": "Verrucomicrobia subdivision 3"
+    }
+  },
+  "return_type": "entry",
+  "request_options": {
+    "paginate": {
+      "start": 0,
+      "rows": 25
+    },
+    "results_content_type": [
+      "experimental"
+    ],
+    "sort": [
+      {
+        "sort_by": "score",
+        "direction": "desc"
+      }
+    ],
+    "scoring_strategy": "combined"
+  }
 }
+"""
 
-EXPECTED_IDS = ['1DWL', '1E08', '1GX7', '1OLN', '1UR6', '1VYC']
+TEST_QUERY_COMBO = """{
+  "query": {
+    "type": "terminal",
+    "label": "text",
+    "service": "text",
+    "parameters": {
+      "attribute": "rcsb_entity_source_organism.taxonomy_lineage.name",
+      "operator": "exact_match",
+      "negation": false,
+      "value": "Verrucomicrobia subdivision 3"
+    }
+  },
+  "return_type": "entry",
+  "request_options": {
+    "paginate": {
+      "start": 0,
+      "rows": 25
+    },
+    "results_content_type": [
+      "computational",
+      "experimental"
+    ],
+    "sort": [
+      {
+        "sort_by": "score",
+        "direction": "desc"
+      }
+    ],
+    "scoring_strategy": "combined"
+  }
+}
+"""
+
+EXPECTED_COMBO = """{
+	"query_id": "9aac1a6b-cdc5-4e30-8e81-718b331ce9b3",
+	"result_type": "entry",
+	"total_count": 2,
+	"result_set": [
+		{
+			"identifier": "AF_AFB9XEI9F1",
+			"score": 1
+		},
+		{
+			"identifier": "AF_AFB9XEK4F1",
+			"score": 1
+		}
+	]
+}"""
 
 
-def test_retrieve_pdb_ids():
+# The expected PDB IDs for the query above, which include also computational results, are from the AlphaFold DB.
+EXPECTED_IDS_AF = ['AF_AFB9XEI9F1', 'AF_AFB9XEK4F1']
+
+
+def test_retrieve_pdb_ids_zero():
     """
     Perform a known JSON test query and check that the expected PDB IDs are returned.
 
     This is an API test, not a unit test.
-    This query retrieve IDs of proteins whose the experimental method is THEORETICAL MODEL (I choose this because it returns only 6 entries, currently).
-    NB: obviously, this test will fail when the number of entries change.
+    This query retrieve IDs of proteins for an organism for which there are no experimental PDB entries.
+    WARNING: obviously, this test will fail when the number of entries change.
 
     :return: None
     """
-    ids = retrieve_pdb_ids(TEST_QUERY)
-    assert ids == EXPECTED_IDS
+    ids = retrieve_pdb_ids(TEST_QUERY_EXP)
+    assert ids == []
+
+
+def test_retrieve_pdb_ids_computational():
+    """
+    Perform a known JSON test query and check that the expected PDB IDs are returned.
+
+    This is an API test, not a unit test.
+    This query retrieve IDs of proteins for an organism for which there are no experimental PDB entries.
+    WARNING: obviously, this test will fail when the number of entries change.
+
+    :return: None
+    """
+    ids = retrieve_pdb_ids(TEST_QUERY_COMBO)
+    assert ids == EXPECTED_IDS_AF
 
 
 def test_store_pdb_ids():
@@ -118,13 +138,13 @@ def test_store_pdb_ids():
 
     :return: None
     """
-    store_pdb_ids(EXPECTED_IDS, 'test_pdb_ids.txt')
-    with open('test_pdb_ids.txt', 'r', encoding='ascii') as file_pointer:
+    store_pdb_ids(EXPECTED_IDS_AF, 'src/test_pdb_ids.txt')
+    with open('src/test_pdb_ids.txt', 'r', encoding='ascii') as file_pointer:
         ids = file_pointer.read().split('\n')
     # the last line should be empty (the file should end with a newline)
     assert ids[-1] == ''
-    assert ids[:-1] == EXPECTED_IDS
-    os.remove('test_pdb_ids.txt')
+    assert ids[:-1] == EXPECTED_IDS_AF
+    os.remove('src/test_pdb_ids.txt')
 
 
 def test_load_pdb_ids():
@@ -133,10 +153,10 @@ def test_load_pdb_ids():
 
     :return: None
     """
-    store_pdb_ids(EXPECTED_IDS, 'test_pdb_ids.txt')
-    ids = load_pdb_ids('test_pdb_ids.txt')
-    assert ids == EXPECTED_IDS
-    os.remove('test_pdb_ids.txt')
+    store_pdb_ids(EXPECTED_IDS_AF, 'src/test_pdb_ids.txt')
+    ids = load_pdb_ids('src/test_pdb_ids.txt')
+    assert ids == EXPECTED_IDS_AF
+    os.remove('src/test_pdb_ids.txt')
 
 
 def test__load_query():
@@ -145,7 +165,8 @@ def test__load_query():
 
     :return: None
     """
-    query = _load_query('queries/test_query.json')
+    expected = """{"query":{"type":"terminal","label":"text","service":"text","parameters":{"attribute":"rcsb_entity_source_organism.taxonomy_lineage.name","operator":"exact_match","negation":false,"value":"Verrucomicrobia subdivision 3"}},"return_type":"entry","request_options":{"paginate":{"start":0,"rows":25},"results_content_type":["computational","experimental"],"sort":[{"sort_by":"score","direction":"desc"}],"scoring_strategy":"combined"}}"""
+    query = _load_query('src/test_query_csm.json')
     # check that is one line and that matches the expected query
     assert len(query.split('\n')) == 1
-    assert json.loads(query) == json.loads(EXPECTED_QUERY)
+    assert json.loads(query) == json.loads(expected)
