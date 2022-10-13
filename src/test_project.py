6339f617288cd794ada66c9c43cebaffdc33b7fe
iacopo <iacopomarmorini@gmail.com>
2022-10-13 16:07:42 +0200

    test_project: test the obsolete files handling

--- src/test_project.py
+++ src/test_project.py
@@ -1,6 +1,10 @@
 """
 Unit tests for the project module.
 """
+# Standard Library
+import os
+from pathlib import Path
+
 # 3rd party
 import pytest
 import responses
@@ -26,6 +30,21 @@ def empty_project(tmp_path):
     query_file1.write_text(HS_QUERY)
     query_file2.write_text(RN_QUERY)
     return Project(tmp_path)
+
+
+@pytest.fixture
+def project_with_files(empty_project):  # pylint: disable=redefined-outer-name
+    """Return a project with some downloaded files."""
+    # add pdb.gz files inside the project directory
+    hs01 = Path(empty_project.data_dir, 'hs01.pdb.gz')
+    hs02 = Path(empty_project.data_dir, 'hs02.pdb.gz')
+    rn01 = Path(empty_project.data_dir, 'rn01.pdb.gz')
+    rn02 = Path(empty_project.data_dir, 'rn02.pdb.gz')
+    hs01.write_text('hs01', encoding='ascii')
+    hs02.write_text('hs02', encoding='ascii')
+    rn01.write_text('rn01', encoding='ascii')
+    rn02.write_text('rn02', encoding='ascii')
+    return empty_project
 
 
 @pytest.fixture
@@ -101,3 +120,35 @@ def test_second_fetch_same_results(empty_project,  # pylint: disable=redefined-o
 
     # The second fetch should not call requests.get() (because the ids are loaded from the local cache).
     assert len(mocked_responses.calls) == 2
+
+
+def test_removed_handling(project_with_files, mocked_responses):  # pylint: disable=redefined-outer-name
+    """
+    Test that the remote-removed ids are handled properly.
+    """
+    mocked_responses.get(
+        url=f'{SEARCH_ENDPOINT_URI}',
+        body='{"result_set": [{"identifier": "hs01"}, {"identifier": "hs02"}]}',
+        status=200,
+        content_type="application/json",
+    )
+    # The remote server returns a different set of ids (rn02 is removed).
+    mocked_responses.get(
+        url=f'{SEARCH_ENDPOINT_URI}',
+        body='{"result_set": [{"identifier": "rn01"}]}',
+        status=200,
+        content_type="application/json",
+    )
+
+    fetch_result = project_with_files.fetch()
+    assert fetch_result.tbd_ids == []
+    assert fetch_result.removed_ids == ['rn02']
+
+    # The requests.get() method should be called two times, since there are two queries.
+    assert len(mocked_responses.calls) == 2
+
+    assert sorted(os.listdir(project_with_files.data_dir)) == [
+        'hs01.pdb.gz', 'hs02.pdb.gz', 'rn01.pdb.gz', 'rn02.pdb.gz']
+    project_with_files.handle_removed(fetch_result)
+    assert sorted(os.listdir(project_with_files.data_dir)) == [
+        'hs01.pdb.gz', 'hs02.pdb.gz', 'rn01.pdb.gz', 'rn02.pdb.gz.obsolete']
