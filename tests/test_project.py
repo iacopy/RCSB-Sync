a8d03e3757923aea60eb3a2b2004001535d6fe7f
iacopo <iacopomarmorini@gmail.com>
2022-11-14 10:15:12 +0100

    Refactoring renaming cleanup fixup
    rename residual "updiff" and "diffs" in test names and locals
    modules: project and test_project
    Close #58

--- tests/test_project.py
+++ tests/test_project.py
@@ -8,51 +8,51 @@ import os
 import project
 
 
-def test_updiff_the_first_time(new_project_dir, remote_server):
+def test_get_status_the_first_time(new_project_dir, remote_server):
     """
     Test that the first time the project check for updates, all the remote ids are considered to be downloaded.
     """
     new_project = project.Project(new_project_dir)
-    diffs = new_project.get_status()
+    status = new_project.get_status()
 
-    assert diffs["Homo_sapiens"].tbd_ids == ["hs01", "hs02", "hs03"]
-    assert diffs["Homo_sapiens"].removed_ids == []
-    assert diffs["Rattus_norvegicus"].tbd_ids == ["rn01", "rn02"]
-    assert diffs["Rattus_norvegicus"].removed_ids == []
+    assert status["Homo_sapiens"].tbd_ids == ["hs01", "hs02", "hs03"]
+    assert status["Homo_sapiens"].removed_ids == []
+    assert status["Rattus_norvegicus"].tbd_ids == ["rn01", "rn02"]
+    assert status["Rattus_norvegicus"].removed_ids == []
 
 
-def test_updiff_resume_rn(project_with_hs_files_gz, remote_server):
+def test_get_status_resume_rn(project_with_hs_files_gz, remote_server):
     """
     Test that resuming a download works properly (Homo sapiens is already downloaded).
     """
-    diffs = project_with_hs_files_gz.get_status()
-    assert diffs["Homo_sapiens"].tbd_ids == []
-    assert diffs["Homo_sapiens"].removed_ids == []
-    assert diffs["Rattus_norvegicus"].tbd_ids == ["rn01", "rn02"]
-    assert diffs["Rattus_norvegicus"].removed_ids == []
+    status = project_with_hs_files_gz.get_status()
+    assert status["Homo_sapiens"].tbd_ids == []
+    assert status["Homo_sapiens"].removed_ids == []
+    assert status["Rattus_norvegicus"].tbd_ids == ["rn01", "rn02"]
+    assert status["Rattus_norvegicus"].removed_ids == []
 
 
-def test_updiff_resume_hs(project_with_rn_files, remote_server):
+def test_get_status_resume_hs(project_with_rn_files, remote_server):
     """
     Test that resuming a download works properly (Rattus norvegicus is already downloaded).
     """
-    diffs = project_with_rn_files.get_status()
-    assert diffs["Homo_sapiens"].tbd_ids == ["hs01", "hs02", "hs03"]
-    assert diffs["Homo_sapiens"].removed_ids == []
-    assert diffs["Rattus_norvegicus"].tbd_ids == []
-    assert diffs["Rattus_norvegicus"].removed_ids == []
+    status = project_with_rn_files.get_status()
+    assert status["Homo_sapiens"].tbd_ids == ["hs01", "hs02", "hs03"]
+    assert status["Homo_sapiens"].removed_ids == []
+    assert status["Rattus_norvegicus"].tbd_ids == []
+    assert status["Rattus_norvegicus"].removed_ids == []
 
 
-def test_updiff_uptodate(project_with_files, remote_server):
+def test_get_status_uptodate(project_with_files, remote_server):
     """
-    Test that the updiff method returns an empty list of ids if the local data is up-to-date.
+    Test that the get_status method returns an empty list of ids if the local data is up-to-date.
     """
     # Check for updates.
-    updiff_result = project_with_files.get_status()
-    assert updiff_result["Homo_sapiens"].tbd_ids == []
-    assert updiff_result["Homo_sapiens"].removed_ids == []
-    assert updiff_result["Rattus_norvegicus"].tbd_ids == []
-    assert updiff_result["Rattus_norvegicus"].removed_ids == []
+    status = project_with_files.get_status()
+    assert status["Homo_sapiens"].tbd_ids == []
+    assert status["Homo_sapiens"].removed_ids == []
+    assert status["Rattus_norvegicus"].tbd_ids == []
+    assert status["Rattus_norvegicus"].removed_ids == []
 
 
 def test_mark_removed(project_with_files, remote_server_changed):
@@ -60,10 +60,10 @@ def test_mark_removed(project_with_files, remote_server_changed):
     Test that obsolete files are properly marked.
     """
     # Check for updates.
-    updiff_result = project_with_files.get_status()
+    status = project_with_files.get_status()
 
     # The sync should mark the files removed from the server as obsolete.
-    project_with_files.do_sync(updiff_result, n_jobs=1)
+    project_with_files.do_sync(status, n_jobs=1)
 
     assert sorted(
         os.listdir(os.path.join(project_with_files.data_dir, "Homo_sapiens"))
@@ -90,10 +90,10 @@ def test_mark_removed_af2(
     Test that obsolete files are properly marked.
     """
     # Check for updates.
-    updiff_result = project_with_af2_volvox_files.get_status()
+    status = project_with_af2_volvox_files.get_status()
 
     # The sync should mark the files removed from the server as obsolete.
-    project_with_af2_volvox_files.do_sync(updiff_result, n_jobs=1)
+    project_with_af2_volvox_files.do_sync(status, n_jobs=1)
 
     assert sorted(
         os.listdir(os.path.join(project_with_af2_volvox_files.data_dir, "Volvox"))
